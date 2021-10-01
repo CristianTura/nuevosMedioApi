9373405c7e3307320f6747c0d4d06db502e738b2
CristianTura <cristianbuenaventura15@gmail.com>
2021-09-30 21:32:07 -0500

    make it better

--- build/keys.js
+++ build/keys.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: ' ',
-        database: 'movies'
+        host: 'bptk5dd1btqmid1a9ave-mysql.services.clever-cloud.com',
+        user: 'uvi2mlmvl36awp2j',
+        password: 'qEDOnHAdnM0eSBU0InL4',
+        database: 'bptk5dd1btqmid1a9ave'
     }
 };
